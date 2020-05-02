4836be1dd63bdb9220ad9291c7552f0b1f9d5e5f
Kevin Keithley <kmk6q@virginia.edu>
2020-05-02 19:16:36 -0400

    Add div for text to be blurred so icon span unaffected

--- js/main.js
+++ js/main.js
@@ -1,17 +1,14 @@
 document.getElementById('blur-text-button').addEventListener('click', function () {
-  const strBtn = 'button_'
-  const strItm = 'item_'
   if (this.classList.contains('game-off')) {
     this.innerHTML = 'Turn off game'
     this.classList.add('game-on')
     this.classList.remove('game-off')
     const items = document.querySelectorAll('.item')
     for (let i = 0; i < items.length; i++) {
-      items[i].classList.add('blurry-text')
-      items[i].setAttribute('id', strItm.concat([i]))
+      items[i].firstElementChild.classList.add('blurry-text')
       var btn = document.createElement('BUTTON')
       btn.innerHTML = 'Show/Hide'
-      btn.setAttribute('id', strBtn.concat([i]))
+      btn.setAttribute('id', 'btn_'.concat([i]))
       btn.setAttribute('onClick', 'toggleClick(this)')
       items[i].appendChild(btn)
     }
@@ -21,12 +18,12 @@ document.getElementById('blur-text-button').addEventListener('click', function (
     this.classList.remove('game-on')
     const items = document.querySelectorAll('.item')
     for (let i = 0; i < items.length; i++) {
-      items[i].classList.remove('blurry-text')
-      var el = document.getElementById(strBtn.concat([i]))
+      items[i].firstElementChild.classList.remove('blurry-text')
+      var el = document.getElementById('btn_'.concat([i]))
       el.remove()
     }
   }
 })
 function toggleClick (id) {
-  id.parentElement.classList.toggle('blurry-text')
+  id.parentElement.firstElementChild.classList.toggle('blurry-text')
 }
